f9798c6d742304bc55382a3d83b758c3b813ca71
Luděk Roleček <ludek@rolecek.cz>
2022-04-28 18:45:53 +0200

    Aktualizace

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,37 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+
 import About from './components/About';
 import Products from './components/Products';
 import Contact from './components/Contact';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <h1>Práce v lekci 11</h1>
-      <About />
-    </>
+
+      <nav>
+        <Link to="/">About</Link>
+        |
+        <Link to="/products">Products</Link>
+        |
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={ <About /> } />
+        <Route path="/products" element={ <Products /> } />
+        <Route path="/contact" element={ <Contact /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
